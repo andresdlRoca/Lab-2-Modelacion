62a3916780adb271989aeac4550f8a7fb7125cbc
andresdlRoca <andresdelaroca11@gmail.com>
2023-08-05 11:54:28 -0600

    Modificacion de cantidad de valores aleatorios + Documentacion

--- leydebil.py
+++ leydebil.py
@@ -1,9 +1,18 @@
 # Ley de debil de los grandes numeros
+'''
+Esta ley establece que cuando hay una secuencia de variables aleatorias independientes
+y distribuidas identicamente dentro de un rango (Como (0,1)), la media de estas variables
+se aproxima a su valor esperado/teorico a medida que el tamaño de la muestra aumenta y se 
+van calculando las medias paciales.
+'''
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 def calcular_medias_parciales(valores):
+    # Las medias parciales tienden a oscilar hacia la media teórica, al inicio
+    # teniendo valores que oscilan bastante pero a medida que se van calculando
+    # las medias parciales se acercan mas a la media teorica
     medias_parciales = np.cumsum(valores) / np.arange(1, len(valores) + 1)
     return medias_parciales
 
@@ -19,10 +28,10 @@ def graficar_medias_parciales(medias_parciales, media_teorica):
     plt.show()
 
 def main():
-    n = 10000
-    valores_aleatorios = np.random.uniform(0, 1, n)
+    n = 1000000  # Cantidad de valores aleatorios a generar
+    valores_aleatorios = np.random.uniform(0, 1, n) # Generacion de n valores aleatorios dentro de (0, 1)
     media_teorica = 0.5  # Media teórica de la distribución uniforme (ų)
-    medias_parciales = calcular_medias_parciales(valores_aleatorios)
+    medias_parciales = calcular_medias_parciales(valores_aleatorios) # Calculo de las medias parciales
     graficar_medias_parciales(medias_parciales, media_teorica)
 
 if __name__ == "__main__":
